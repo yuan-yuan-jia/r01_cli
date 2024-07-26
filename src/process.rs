b5dc01e6f4b922258aba897fd395563e33da3de4
jyy <1305081045@qq.com>
2024-07-27 02:03:42 +0800

    feature:add function of generating file of yaml format

--- src/process.rs
+++ src/process.rs
@@ -2,6 +2,8 @@ use csv::Reader;
 use serde::{Deserialize, Serialize};
 use std::fs;
 
+use crate::opts::OutputFormat;
+
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Player {
     #[serde(rename = "Name")]
@@ -20,14 +22,27 @@ pub struct Player {
     kit: u8,
 }
 
-pub fn process_csv_2_json(filepath: &str, outputfilepath: &str) -> anyhow::Result<()> {
+pub fn process_csv_2_json(
+    filepath: &str,
+    output: String,
+    format: OutputFormat,
+) -> anyhow::Result<()> {
     let mut reader = Reader::from_path(filepath)?;
     let mut ret = Vec::with_capacity(128);
-    for result in reader.deserialize() {
-        let record: Player = result?;
+    let headers = reader.headers()?.clone();
+    for result in reader.records() {
+        let record = result?;
         //println!("{:?}", record)
-        ret.push(record);
+        let json_value = headers
+            .iter()
+            .zip(record.iter())
+            .collect::<serde_json::Value>();
+        ret.push(json_value);
     }
-    let json = serde_json::to_string_pretty(&ret)?;
-    Ok(fs::write(outputfilepath, json)?)
+    let content = match format {
+        OutputFormat::Json => serde_json::to_string_pretty(&ret)?,
+        OutputFormat::Yaml => serde_yaml::to_string(&ret)?,
+    };
+    fs::write(output, content)?;
+    Ok(())
 }
