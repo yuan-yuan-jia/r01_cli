0516752801062cf14534800546303888bfaeea1f
yuan-yuan-jia <1305081045@qq.com>
2024-07-27 02:05:35 +0800

    feature:add function of generating file of yaml format (#5)

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,13 @@ fn main() -> anyhow::Result<()> {
     let opts = Opts::parse();
     match opts.cmd {
         Subcommand::Csv(opts) => {
-            process_csv_2_json(&opts.input, &opts.output)?;
+            let output = if let Some(output) = opts.output {
+                output.clone()
+            } else {
+                format!("output.{}", opts.format)
+            };
+
+            process_csv_2_json(&opts.input, output, opts.format)?;
         }
     }
 
--- src/opts.rs
+++ src/opts.rs
@@ -1,3 +1,6 @@
+use core::fmt;
+use std::str::FromStr;
+
 use clap::Parser;
 
 #[derive(Debug, Parser)]
@@ -18,9 +21,12 @@ pub struct CsvOpts {
     #[arg(short, long, value_parser = verify_input_file)]
     pub input: String,
 
-    #[arg(short, long, default_value = "output.json")]
+    #[arg(short, long)]
     // the default_value'work is  "output.json".into()
-    pub output: String,
+    pub output: Option<String>,
+
+    #[arg(long, value_parser = parser_format, default_value = "json")]
+    pub format: OutputFormat,
 
     #[arg(short, long, default_value_t = ',')] // default_value_t assign directly ',' to delimiter
     pub delimiter: char,
@@ -29,10 +35,47 @@ pub struct CsvOpts {
     pub header: bool,
 }
 
+#[derive(Debug, Clone, Copy)]
+pub enum OutputFormat {
+    Json,
+    Yaml,
+}
+
 fn verify_input_file(filename: &str) -> Result<String, &'static str> {
     if std::path::Path::new(filename).exists() {
         Ok(filename.into())
     } else {
         Err("File does not exists")
+    }
+}
+
+fn parser_format(format: &str) -> Result<OutputFormat, anyhow::Error> {
+    format.parse()
+}
+
+impl FromStr for OutputFormat {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "json" => Ok(OutputFormat::Json),
+            "yaml" => Ok(OutputFormat::Yaml),
+            _ => Err(anyhow::anyhow!("Invalid format")),
+        }
+    }
+}
+
+impl From<OutputFormat> for &'static str {
+    fn from(value: OutputFormat) -> Self {
+        match value {
+            OutputFormat::Json => "json",
+            OutputFormat::Yaml => "yaml",
+        }
+    }
+}
+
+impl fmt::Display for OutputFormat {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", Into::<&str>::into(*self))
     }
 }
