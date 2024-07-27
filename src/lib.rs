4a4da4a93ea51d336276ee7496d8c74cb0b0927c
jyy <1305081045@qq.com>
2024-07-27 12:04:50 +0800

    feature: add base64 function

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
-mod opts;
+mod cli;
 mod process;
 
-pub use opts::{CsvOpts, Opts, Subcommand};
-pub use process::process_csv_2_json;
-pub use process::process_genpass;
+pub use cli::{
+    Base64DecodeOpts, Base64EncodeOpts, Base64Format, Base64SubCommand, CsvOpts, Opts, Subcommand,
+};
+pub use process::{process_csv_2_json, process_decode, process_encode, process_genpass};
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@
 use clap::Parser;
 use rcli::process_csv_2_json;
 use rcli::process_genpass;
-use rcli::{Opts, Subcommand};
+use rcli::{process_decode, process_encode};
+use rcli::{Base64SubCommand, Opts, Subcommand};
 
 fn main() -> anyhow::Result<()> {
     let opts = Opts::parse();
@@ -25,6 +26,14 @@ fn main() -> anyhow::Result<()> {
                 opts.symbol,
             )?;
         }
+        Subcommand::Base64(subcommd) => match subcommd {
+            Base64SubCommand::Encode(encode_opts) => {
+                process_encode(&encode_opts.input, encode_opts.format)?;
+            }
+            Base64SubCommand::Decode(decode_opts) => {
+                process_decode(&decode_opts.input, decode_opts.format)?;
+            }
+        },
     }
 
     Ok(())
